2432a94e7ccd7c395ec153a6bfb6487bd19c29d1
FernandoCaranqui <kevin.caranqui@epn.edu.ec>
2023-01-25 12:42:46 -0500

    Agregando atributos y métodos a la clase
    En esta clase se agregó métodos con los cuales podemos acceder a los layouts en una vista determinar.

---
+++ 01-android/app/src/main/java/com/example/kfcyapplication/FRecyckerViewAdctadorNombreCedula.kt
@@ -0,0 +1,62 @@
+package com.example.kfcyapplication
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+
+class FRecyckerViewAdctadorNombreCedula (
+    private val contexto: GRecyclerView,
+    private val lista: ArrayList<BEntrenador>,
+    private val recyclerView: RecyclerView
+    ): RecyclerView.Adapter<FRecyckerViewAdctadorNombreCedula.MyviewHolder>(){
+        inner class MyviewHolder(view:View): RecyclerView.ViewHolder(view){
+            //ponemos las variables que tenemos en la irtefaz Recycler
+            val nombreTextView: TextView
+            val cedulaTextView: TextView
+            val likesTextView: TextView
+            val accionBUtton: Button
+            var numeroLikes = 0
+
+            init {
+                nombreTextView = view.findViewById(R.id.tv_nombre)
+                cedulaTextView = view.findViewById(R.id.tv_cedula)
+                likesTextView = view.findViewById(R.id.tv_likes)
+                accionBUtton = view.findViewById(R.id.btn_dar_like)
+                accionBUtton.setOnClickListener {anadirLike()}
+            }
+            fun anadirLike(){
+                numeroLikes = numeroLikes +1
+                likesTextView.text = numeroLikes.toString()
+        }
+    }
+
+    //setear el layout que vamos a utilizar
+    override fun onCreateViewHolder(parent: ViewGroup,viewType: Int): MyviewHolder{
+        val itemView = LayoutInflater //
+            .from(parent.context) //obtiene el layoutInflater de un contexto dado
+            .inflate(
+                R.layout.recycler_view_vista,
+                parent,
+                false
+            )
+        return MyviewHolder(itemView)
+    }
+
+    //debemos saber extactmente como llenar los datos
+    //seteamos los datos para la iteracion
+    override fun onBindViewHolder(holder: MyviewHolder, position: Int) {
+        val entrenadorActual = this.lista[position]
+        holder.nombreTextView.text = entrenadorActual.nombre
+        holder.cedulaTextView.text = entrenadorActual.descripcion
+        holder.accionBUtton.text = "Like ${entrenadorActual.nombre}"
+        holder.likesTextView.text = "0"
+    }
+
+    //tamaño del arreglo
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
